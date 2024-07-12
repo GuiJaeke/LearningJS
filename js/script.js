661aec1f695dcc9efba0265be020ab6715d5355a
GuiJaeke <guilherme_jaeke@hotmail.com>
2024-07-12 12:04:42 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+console.log("script linkado na casa do krl")
+
+ var a = 68+97
+ var b = a / 5
+ var batata = "batata"
+ var andre = "andré gosta de "
+ if(b < a) {
+    console.log(a)
+ }
+ else {
+    console.log(b)
+ }
+ console.log(andre + batata)
+ 
+ var verd = true
+ console.log(verd)
+ console.log(typeof verd)
