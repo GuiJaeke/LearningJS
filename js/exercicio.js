f731720d2aaaacea4fd20ff6075386532c754db9
Gui Jaeke <107638175+GuiJaeke@users.noreply.github.com>
2024-07-17 18:00:27 -0300

    criada uma calculadora basica usando apenas meus conhecimentos adquirido até agora com o curso

--- js/exercicio.js
+++ js/exercicio.js
@@ -1,7 +1,7 @@
 function fazli() {
     var criaul = document.createElement('ul');
     var pegalista = document.getElementsByTagName('ul')
-    var insira = document.getElementById('1').addEventListener('click', function () {
+    var insira = document.getElementById('bah').addEventListener('click', function () {
         var criali = document.createElement('li')
         var texto = document.querySelector('#inputtxt')
         gettxt = document.createTextNode(texto.value)
@@ -19,4 +19,99 @@ function fazli() {
 }
 
 fazli()
+
+// espaço //
+var body1 = document.querySelector('body')
+var res = document.querySelector('.res')
+var number = []
+var num1 = document.getElementById('1').addEventListener('click', function(){
+    number.push(1),
+    cl(number)
+}
+)
+var num2 = document.getElementById('2').addEventListener('click', function(){
+    number.push(2)
+
+    cl(number)
+}
+)
+var num3 = document.getElementById('3').addEventListener('click', function(){
+    number.push(3),
+    cl(number)
+}
+)
+var num4 = document.getElementById('4').addEventListener('click', function(){
+    number.push(4)
+
+    cl(number)
+}
+)
+var num5 = document.getElementById('5').addEventListener('click', function(){
+    number.push(5),
+    cl(number)
+}
+)
+var num6 = document.getElementById('6').addEventListener('click', function(){
+    number.push(6)
+
+    cl(number)
+}
+)
+var num7 = document.getElementById('7').addEventListener('click', function(){
+    number.push(7),
+    cl(number)
+}
+)
+var num8 = document.getElementById('8').addEventListener('click', function(){
+    number.push(8)
+
+    cl(number)
+}
+)
+var num9 = document.getElementById('9').addEventListener('click', function(){
+    number.push(9),
+    cl(number)
+}
+)
+var num0 = document.getElementById('0').addEventListener('click', function(){
+    number.push(0)
+
+    cl(number)
+}
+)
+cl(number)
+var soma = document.getElementById('soma').addEventListener('click', function (){
+    let sum = document.createElement('p')
+    let print = document.createTextNode(number[0] + number[1])
+    cl(print)
+    
+    sum.appendChild(print)
+    res.appendChild(sum)
+})
+var sub = document.getElementById('sub').addEventListener('click', function (){
+    let sum = document.createElement('p')
+    let print = document.createTextNode(number[0] - number[1])
+    cl(print)
+    
+    sum.appendChild(print)
+    res.appendChild(sum)
+})
+var mult = document.getElementById('mult').addEventListener('click', function (){
+    let sum = document.createElement('p')
+    let print = document.createTextNode(number[0] * number[1])
+    cl(print)
+    
+    sum.appendChild(print)
+    res.appendChild(sum)
+})
+var divis = document.getElementById('divis').addEventListener('click', function (){
+    let sum = document.createElement('p')
+    let print = document.createTextNode(number[0] / number[1])
+    cl(print)
+    
+    sum.appendChild(print)
+    res.appendChild(sum)
+})
+
+
 
--- js/script.js
+++ js/script.js
@@ -1,331 +1,341 @@
-console.log("script linkado na casa do krl")
+// console.log("script linkado na casa do krl")
 
- var a = 68+97
- var b = a / 5
- var batata = "batata"
- var andre = "andré gosta de "
- var nome = "guilherme"
- var idade = 1
- if(b < a) {
-    console.log(a)
- }
- else {
-    console.log(b)
- }
- console.log(andre + batata)
+//  var a = 68+97
+//  var b = a / 5
+//  var batata = "batata"
+//  var andre = "andré gosta de "
+//  var nome = "guilherme"
+//  var idade = 1
+//  if(b < a) {
+//     console.log(a)
+//  }
+//  else {
+//     console.log(b)
+//  }
+//  console.log(andre + batata)
  
- var verd = true
- console.log(verd)
- console.log(typeof verd)
-
- var arr = ["guitop", 22, "olhador", true]
- console.log(arr[1])
- console.log(arr)
-
- var form = {
-   nome: "guilherme",
-   idade: 22,
-   profissao: "top demais"
- }
- if (form.idade > 18) {
-   console.log("acesso permitido ao site!")
- }
- else {
-   console.log("acesso negado!")
- }
- console.log(form)
- var num = '5';
-
- if (num == 5) {
-   console.log("mesma função")
- }
- if (num !== 5) {
-   console.log("não é a mesma função");
-   }
- if (nome == "guilherme" && idade >= 18) {
-   console.log("que coisa linda cara, meu deus!!")
- }
- else{
-   console.log("não foi otário")
- }
- if (nome == "guilherm" || idade >= 18) {
-   console.log("que coisa linda cara, meu deus!!")
- }
- else{
-   console.log("não foi otário")
- }
- if (!(nome == "guilherm" && idade >= 18)) {
-   console.log("que coisa linda cara, meu deus!!")
- }
- else{
-   console.log("não foi otário")
- }
- while (idade < 18) {
-  idade = idade + 1,
-  console.log(idade)
- }
- console.log(idade)
-// While e For
- var x = 0
- while (x <= 3) {
-  console.log(arr[x])
-  x = x +1
- }
- for (var i = 0; i < arr.length; i++) {
-  console.log(arr[i])
- }
-function primfunc() {
-  console.log(form.idade)
- }
- primfunc()
+//  var verd = true
+//  console.log(verd)
+//  console.log(typeof verd)
+
+//  var arr = ["guitop", 22, "olhador", true]
+//  console.log(arr[1])
+//  console.log(arr)
+
+//  var form = {
+//    nome: "guilherme",
+//    idade: 22,
+//    profissao: "top demais"
+//  }
+//  if (form.idade > 18) {
+//    console.log("acesso permitido ao site!")
+//  }
+//  else {
+//    console.log("acesso negado!")
+//  }
+//  console.log(form)
+//  var num = '5';
+
+//  if (num == 5) {
+//    console.log("mesma função")
+//  }
+//  if (num !== 5) {
+//    console.log("não é a mesma função");
+//    }
+//  if (nome == "guilherme" && idade >= 18) {
+//    console.log("que coisa linda cara, meu deus!!")
+//  }
+//  else{
+//    console.log("não foi otário")
+//  }
+//  if (nome == "guilherm" || idade >= 18) {
+//    console.log("que coisa linda cara, meu deus!!")
+//  }
+//  else{
+//    console.log("não foi otário")
+//  }
+//  if (!(nome == "guilherm" && idade >= 18)) {
+//    console.log("que coisa linda cara, meu deus!!")
+//  }
+//  else{
+//    console.log("não foi otário")
+//  }
+//  while (idade < 18) {
+//   idade = idade + 1,
+//   console.log(idade)
+//  }
+//  console.log(idade)
+// // While e For
+//  var x = 0
+//  while (x <= 3) {
+//   console.log(arr[x])
+//   x = x +1
+//  }
+//  for (var i = 0; i < arr.length; i++) {
+//   console.log(arr[i])
+//  }
+// function primfunc() {
+//   console.log(form.idade)
+//  }
+//  primfunc()
 
-function dizerNome(nome) {
-  console.log(nome)
+// function dizerNome(nome) {
+//   console.log(nome)
 
- }
-dizerNome("guizindabros")
+//  }
+// dizerNome("guizindabros")
 
-function soma (a, b) {
-  var soma = a + b
-  return soma
-}
+// function soma (a, b) {
+//   var soma = a + b
+//   return soma
+// }
 
-var res = soma (55, 88)
-console.log(res)
+// var res = soma (55, 88)
+// console.log(res)
 
-//                 DOM                 //
-function ulSite() {
-var lista = ['bros', 'titan', 'fan', 'start', 'hornet']
+// //                 DOM                 //
+// function ulSite() {
+// var lista = ['bros', 'titan', 'fan', 'start', 'hornet']
 
-var listaul = document.createElement('ul'); // cria uma ul basica no HTML
+// var listaul = document.createElement('ul'); // cria uma ul basica no HTML
 
-var body = document.getElementsByTagName('body'); // pega o 'body' criado em HTML e insere no JS
+// var body = document.getElementsByTagName('body'); // pega o 'body' criado em HTML e insere no JS
 
-body[0].appendChild(listaul) // insere a ul criada na var listaul no 'body' do HTML
+// body[0].appendChild(listaul) // insere a ul criada na var listaul no 'body' do HTML
 
-var listaNoBody = document.getElementsByTagName('ul') // pega a ul basica criada na var listaul
+// var listaNoBody = document.getElementsByTagName('ul') // pega a ul basica criada na var listaul
 
-for (var i = 0; i < lista.length; i++) {
-  var liFor = document.createElement('li'); // cria uma li basica para cada indice da lista
+// for (var i = 0; i < lista.length; i++) {
+//   var liFor = document.createElement('li'); // cria uma li basica para cada indice da lista
 
-  var textLI = document.createTextNode(lista[i]); //escreve cada indice da lista e fica armazenado no textLI
+//   var textLI = document.createTextNode(lista[i]); //escreve cada indice da lista e fica armazenado no textLI
 
-  liFor.appendChild(textLI); // pega o que ta escrito no textLI e coloca na li basica criada na primeira variável do 'for'
+//   liFor.appendChild(textLI); // pega o que ta escrito no textLI e coloca na li basica criada na primeira variável do 'for'
 
-  listaNoBody[0].appendChild(liFor); // acrescenta o li com o texto pronto na ul basica criada pela variável listaNoBody fora do 'for'
+//   listaNoBody[0].appendChild(liFor); // acrescenta o li com o texto pronto na ul basica criada pela variável listaNoBody fora do 'for'
 
-}
-}
+// }
+// }
 
-let y = 3;
+// let y = 3;
 
-y = 33;
+// y = 33;
 
-const z = 50
+// const z = 50
 
-console.log(y)
-if (true) {
-  let y = 72
-  console.log(y)
-}
+// console.log(y)
+// if (true) {
+//   let y = 72
+//   console.log(y)
+// }
 
-// parsefloat
+// // parsefloat
 
-console.log(parseFloat('10.56544'))
-console.log(Number.parseFloat('10.56544'))
+// console.log(parseFloat('10.56544'))
+// console.log(Number.parseFloat('10.56544'))
 
-// parseint
+// // parseint
 
-console.log(parseInt('52'))
-console.log(parseInt(2.688))
+// console.log(parseInt('52'))
+// console.log(parseInt(2.688))
 
-// tofixed
+// // tofixed
 
-console.log(25.65885.toFixed(2))
+// console.log(25.65885.toFixed(2))
 
-// isNaN
+// // isNaN
 
-console.log(isNaN("teste"))
-console.log(isNaN("55"))
-console.log(isNaN(55))
+// console.log(isNaN("teste"))
+// console.log(isNaN("55"))
+// console.log(isNaN(55))
 
-//MAX_VALUE e MIN_VALUE
+// //MAX_VALUE e MIN_VALUE
 
-console.log(Number.MAX_VALUE)
-console.log(Number.MIN_VALUE)
+// console.log(Number.MAX_VALUE)
+// console.log(Number.MIN_VALUE)
 
-// indexOf
+// // indexOf
 
-lista = "frase top demais ta louco demais"
+// lista = "frase top demais ta louco demais"
 
-console.log(lista.indexOf("demais"))
+// console.log(lista.indexOf("demais"))
 
-// slice
+// // slice
 
-var ret = lista.slice(10, 16)
+// var ret = lista.slice(10, 16)
 
-console.log(ret)
+// console.log(ret)
 
-// replace
+// // replace
 
-var rep = lista.replace("demais", "demasiado")
+// var rep = lista.replace("demais", "demasiado")
 
-console.log(rep)
+// console.log(rep)
 
-//toLowerCase e ToUpperCase
+// //toLowerCase e ToUpperCase
 
-var frase = "Dede10 não é poderoso fodão"
+// var frase = "Dede10 não é poderoso fodão"
 
-console.log(frase.toLowerCase())
-console.log(frase.toUpperCase())
+// console.log(frase.toLowerCase())
+// console.log(frase.toUpperCase())
 
-// trim
+// // trim
 
-var nometrim = "     dedeu      "
-var mod = nometrim.trim()
+// var nometrim = "     dedeu      "
+// var mod = nometrim.trim()
 
 
-console.log(nometrim)
-console.log(mod)
+// console.log(nometrim)
+// console.log(mod)
 
-// split
+// // split
 
-listasplit = lista.split(" ")
-console.log(listasplit)
-console.log(listasplit[1])
+// listasplit = lista.split(" ")
+// console.log(listasplit)
+// console.log(listasplit[1])
 
-// lastIndexOf
+// // lastIndexOf
 
-console.log(lista.lastIndexOf("demais"))
+// console.log(lista.lastIndexOf("demais"))
 
-// push
+// // push
 
-listasplit.push("guimaneirão")
+// listasplit.push("guimaneirão")
 
-console.log(listasplit)
+// console.log(listasplit)
 
-// pop
+// // pop
 
-listasplit.pop()
-listasplit.pop()
+// listasplit.pop()
+// listasplit.pop()
 
-console.log(listasplit)
+// console.log(listasplit)
 
-// unshift
+// // unshift
 
-listasplit.unshift("essa")
+// listasplit.unshift("essa")
 
-console.log(listasplit)
+// console.log(listasplit)
 
-// função console.log
+// // função console.log
 
-function cl(a) {
-  console.log(a)
-}
+// function cl(a) {
+//   console.log(a)
+// }
 
-cl(listasplit[5])
+// cl(listasplit[5]);
 
-// shift
+// // shift
 
-listasplit.shift()
+// listasplit.shift()
 
-cl(listasplit)
+// cl(listasplit)
 
-// splice
+// // splice
 
-listasplit.splice(3, 0, "você")
-cl(listasplit)
+// listasplit.splice(3, 0, "você")
+// cl(listasplit)
 
-// join
+// // join
 
-cl(listasplit.join(" "))
+// cl(listasplit.join(" "))
 
-// objetos e this
+// // objetos e this
 
-let pessoa = {
-  nome: "marqueens",
-  idade: 32,
-  falar: function() {
-    cl("olá mi amigo")
-  },
-  sum: function (a, b) {
-    return a + b
-  },
-  dznome: function() {
-    console.log("O meu nome é " + this.nome)
-  }
-};
+// let pessoa = {
+//   nome: "marqueens",
+//   idade: 32,
+//   falar: function() {
+//     cl("olá mi amigo")
+//   },
+//   sum: function (a, b) {
+//     return a + b
+//   },
+//   dznome: function() {
+//     console.log("O meu nome é " + this.nome)
+//   }
+// };
 
-cl(pessoa)
-pessoa.dznome()
-let sum = pessoa.sum(65, 22);
+// cl(pessoa)
+// pessoa.dznome()
+// let sum = pessoa.sum(65, 22);
 
-cl(sum)
+// cl(sum)
 
-// acessando tags
+// // acessando tags
 
-var lis = document.getElementsByTagName('li')
+// var lis = document.getElementsByTagName('li')
 
-cl(lis)
+// cl(lis)
 
-// querySelector e querySelectorAll
+// // querySelector e querySelectorAll
 
-var liqs = document.querySelector('ul')
+// var liqs = document.querySelector('ul')
 
-cl(liqs)
+// cl(liqs)
 
-var liqs = document.querySelectorAll('li')
+// var liqs = document.querySelectorAll('li')
 
-cl(liqs)
+// cl(liqs)
 
-//textContent e innerHTML
+// //textContent e innerHTML
 
-var alth1 = document.querySelector('h1')
-cl(alth1)
+// var alth1 = document.querySelector('h1')
+// cl(alth1)
 
-alth1.innerHTML = "foi quase vai"
+// alth1.innerHTML = "foi quase vai"
 
-var alth2 = document.querySelector('h2')
-cl(alth1)
+// var alth2 = document.querySelector('h2')
+// cl(alth1)
 
-alth2.textContent = "parte2"
+// alth2.textContent = "parte2"
 
-ulSite()
+// ulSite()
 
-// criando novos elementos via DOM
+// // criando novos elementos via DOM
 
-var newP = document.createElement('p')
-var textoP = document.createTextNode("Ao observar as intenções das pessoas no dia a dia")
+// /*var newP = document.createElement('p')
+// var textoP = document.createTextNode("Ao observar as intenções das pessoas no dia a dia")
 
-newP.appendChild(textoP)
+// newP.appendChild(textoP)
 
-var body = document.querySelector('body')
+// var body = document.querySelector('body')
 
-body.appendChild(newP)
+// body.appendChild(newP)
+// */
+// // inserindo em div
 
-// inserindo em div
+// var divi = document.querySelector('div')
+// var newP2 = document.createElement('p')
+// //var textoP2 = document.createTextNode("denota-se uma falta de compaixão pelo próximo")
+// newP2.appendChild(textoP2)
+// divi.appendChild(newP2)
 
-var divi = document.querySelector('div')
-var newP2 = document.createElement('p')
-var textoP2 = document.createTextNode("denota-se uma falta de compaixão pelo próximo")
-newP2.appendChild(textoP2)
-divi.appendChild(newP2)
+// // removeChild e remove
+// //divi.removeChild(newP2)
 
-// removeChild e remove
-//divi.removeChild(newP2)
+// var estouro = document.querySelector('h2')
 
-var estouro = document.querySelector('h2')
+// estouro.remove()
 
-estouro.remove()
+// // diferença appendChild e insertBefore
 
-// diferença appendChild e insertBefore
+// var criaDiv = document.createElement('div')
 
-var criaDiv = document.createElement('div')
+// criaDiv.classList = "js"
 
-criaDiv.classList = "js"
+// criaDiv.appendChild(newP2)
 
-criaDiv.appendChild(newP2)
+// cl(criaDiv)
 
-cl(criaDiv)
+// divi.appendChild(criaDiv)
 
-divi.appendChild(criaDiv)
+
+array = [1,2,3,4, 5, 6, 7]
+
+let lista = array.filter((numero) => {
+
+  return numero % 2 == 1
+})
+
+console.log(lista);
