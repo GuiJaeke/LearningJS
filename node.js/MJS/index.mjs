1cf4c94475acfe9a68a6febe39fd6ca08553b970
Gui Jaeke <107638175+GuiJaeke@users.noreply.github.com>
2024-07-18 17:47:53 -0300

    Entendendo os conceitos de modulos e realizando alguns exemplos

---
+++ node.js/MJS/index.mjs
@@ -0,0 +1,6 @@
+import soma from './modulo.mjs'
+
+
+soma(2, 5)
+
+soma(5, 28)
---
+++ node.js/index.js
@@ -0,0 +1,13 @@
+var modulo = require('./node.js')
+
+const soma = modulo.soma
+
+soma(2, 5)
+
+soma(5,8)
+
+var path = require('path')
+
+var extension = path.extname('arquivo.xml')
+
+console.log(extension)
