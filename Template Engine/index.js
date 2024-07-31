4c148c5d998586ce55ee6e78b0924e83afb3c2a7
guijaeke <guilherme_jaeke@hotmail.com>
2024-07-31 00:34:36 -0300

    Criando dados no frontEnd para backEnd e retornando tratado de volta no frontEnd

--- Template Engine/index.js
+++ Template Engine/index.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const { engine } = require('express-handlebars'); // Importando o engine da forma correta
-
+const router = express.Router()
 const app = express();
 
 
 // Configuração do motor de visualização Handlebars
 app.engine('handlebars', engine()); // Usando engine() em vez de exphbs()
 app.set('view engine', 'handlebars');
+
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json())
 
 app.use(express.static('public'))
 
@@ -30,6 +38,28 @@ app.get('/dashboard', (req, res)=>{
     res.render('dashboard', {items})
 })
 
+app.get('/form', (req, res)=>{
+
+    res.render('userform')
+})
+
+app.post('/users/save', (req, res) =>{
+    const cdt = {
+        nome: req.body.name,
+        senha: req.body.senha
+    }
+    var userLogin = undefined
+    if(cdt.nome == 'tooooma'){
+        let userLogin = true
+        console.log(userLogin)
+    }else {
+        let userLogin = false
+        console.log(userLogin)
+    }
+    console.log(cdt)
+    res.render('dashboard', {cdt})
+})
+
 app.get('/post', (req, res) =>{
     const post = {
         title: 'aprender ser o Alan Turing',
@@ -37,12 +67,7 @@ app.get('/post', (req, res) =>{
         body: 'este artigo é um tchupreks and um tchuprrey',
         comments: 4
     }
-
-
-
-
     res.render('blogpost', {post})
-
 }
     )
 
