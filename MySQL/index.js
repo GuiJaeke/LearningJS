bd2547d9787367153e46f87e22623fcb7c5844d0
Gui Jaeke <107638175+GuiJaeke@users.noreply.github.com>
2024-08-05 17:41:30 -0300

    Resgatando dados do banco de dados e enviando de forma organizada para pagina

--- MySQL/index.js
+++ MySQL/index.js
@@ -3,13 +3,51 @@ const { engine } = require('express-handlebars')
 const mysql = require('mysql')
 const app = express()
 
+app.use(
+    express.urlencoded({
+            extended: true
+        })
+)
+
+app.use(express.json())
+
 app.engine('handlebars', engine())
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) =>{
+app.get('/home', (req, res) =>{
     res.render('home')
+})
+
+app.post('/book/insert', (req, res) => {
+    const title = req.body.title
+    const page = req.body.page
+    const sql = `INSERT INTO BOOKS (TITLE, PAGE) VALUES ('${title}', '${page}')`
+
+    conn.query(sql, function(err){
+        if (err) {
+            console.log(err)
+        }
+
+        res.redirect('/home')
+    })
+})
+
+app.get('/books', (req, res) =>{
+    const sql2 = `SELECT * FROM books;`
+
+    conn.query(sql2, function(err, data) {
+
+        if (err) {
+            console.log(err)
+            return
+        }
+        const books = data
+
+        console.log(books)
+        res.render('books', { books })
+    })
 })
 
 const conn = mysql.createConnection({
@@ -17,6 +55,10 @@ const conn = mysql.createConnection({
     user: 'root',
     password: '',
     database: 'BD'
+})
+
+app.use(function(req, res, next){
+    res.status(404).render(`404`)
 })
 
 conn.connect(function(err) {
