d256f7cb3149734b880e4357a7a0b54e8c457a30
Gui Jaeke <107638175+GuiJaeke@users.noreply.github.com>
2024-08-06 16:42:59 -0300

    Simplificando conexão com o connection pool

--- MySQL/index.js
+++ MySQL/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const { engine } = require('express-handlebars')
-const mysql = require('mysql')
+const pool = require('./BD/conn')
 const app = express()
 
 app.use(
@@ -19,7 +19,7 @@ app.set('view engine', 'handlebars')
 app.use(express.static('public'))
 
 app.get('/home', (req, res) =>{
-    res.render('home', { alert: 'Livro cadastrado no Banco de dados' })
+    res.render('home')
 })
 
 app.post('/book/insert', (req, res) => {
@@ -27,7 +27,7 @@ app.post('/book/insert', (req, res) => {
     const page = req.body.pagess
     const sql = `UPDATE INTO BOOKS (TITLE, PAGE) VALUES ('${title}', '${page}')`
  
-    conn.query(sql, function(err){
+    pool.query(sql, function(err){
         if (err) {
             console.log(err)
         }
@@ -40,7 +40,7 @@ app.post('/book/insert', (req, res) => {
 app.get('/books', (req, res) =>{
     const sql2 = `SELECT * FROM books;`
 
-    conn.query(sql2, function(err, data) {
+    pool.query(sql2, function(err, data) {
 
         if (err) {
             console.log(err)
@@ -57,7 +57,7 @@ app.post('/search', (req, res) =>{
 
     const sql3 = `SELECT * FROM books WHERE ID = '${id}';`
 
-    conn.query(sql3, function(err, data) {
+    pool.query(sql3, function(err, data) {
 
         if (err) {
             console.log(err)
@@ -73,7 +73,7 @@ app.get('/edit/:id', (req, res) =>{
 
     const sql4 = `SELECT * FROM books WHERE ID = '${id}';`
 
-    conn.query(sql4, function(err, data) {
+    pool.query(sql4, function(err, data) {
 
         if (err) {
             console.log(err)
@@ -90,7 +90,7 @@ app.post('/edit/post', (req, res) => {
     const page = req.body.page
     const sql = `UPDATE books SET TITLE = '${title}', PAGE = '${page}' WHERE ID = ${id}`
  
-    conn.query(sql, function(err){
+    pool.query(sql, function(err){
         if (err) {
             console.log(err)
         }
@@ -102,7 +102,7 @@ app.post('/delete/:id', (req, res) => {
     const id = req.params.id
     const sql = `DELETE FROM books WHERE ID = ${id}`
  
-    conn.query(sql, function(err){
+    pool.query(sql, function(err){
         if (err) {
             console.log(err)
         }
@@ -111,24 +111,9 @@ app.post('/delete/:id', (req, res) => {
     })
 })
 
-
-const conn = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'BD'
-})
-
 app.use(function(req, res, next){
     res.status(404).render(`404`)
 })
-
-conn.connect(function(err) {
-    if(err) {
-        console.log(err)
-    }
-
-    console.log("Conectou ao MySQL!")
-    app.listen(3000)
-    console.log("sistema rodando na porta 3000!")
-})
+console.log("Conectou ao MySQL!")
+console.log("sistema rodando na porta 3000!")
+app.listen(3000)
